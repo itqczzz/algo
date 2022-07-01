ed310016720944171ad8da101949aa58dcf23c86
qiuchang <qiuchang@digitetown.com>
2022-07-01 15:26:47 +0800

    队列实现树的层次遍历

--- src/tree/BFSTree.java
+++ src/tree/BFSTree.java
@@ -1,11 +1,8 @@
 package tree;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
-public class DFSTree {
+public class BFSTree {
     public static List<List<Integer>> resList = new ArrayList<List<Integer>>();
 
     //DFS--递归方式
@@ -53,6 +50,25 @@ public class DFSTree {
      * @param root
      */
     public static void BFS(TreeNode root){
+        //base check
+        if(root==null){
+            return;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            //探查队列中的元素
+            List<Integer> tempList = new ArrayList<>();
+            int size = queue.size();
+            while(size>0){
+                TreeNode tempNode = queue.poll();
+                tempList.add(tempNode.val);
+                if(tempNode.left!=null) queue.add(tempNode.left);
+                if(tempNode.right!=null) queue.add(tempNode.right);
+                size--;
+            }
+            resList.add(tempList);
+        }
 
     }
 
@@ -66,7 +82,7 @@ public class DFSTree {
         root.right = right;
         right.left = new TreeNode(5);
         right.right = new TreeNode(6);
-        checkFun02(root);
+        BFS(root);
         resList.forEach(System.out::println);
     }
 }
