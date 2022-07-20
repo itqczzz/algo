0a724622702b354510f609224371c60f321b724f
qiuchang <qiuchang@digitetown.com>
2022-07-20 11:39:36 +0800

    二叉树倒序层次遍历

--- src/tree/LevelOrderBottomSolution.java
+++ src/tree/LevelOrderBottomSolution.java
@@ -1,6 +1,9 @@
 package tree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * https://leetcode.cn/problems/binary-tree-level-order-traversal-ii/
@@ -12,6 +15,27 @@ public class LevelOrderBottomSolution {
      * @return
      */
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null){
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> temp = new LinkedList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                temp.add(node.val);
+                if(node.left !=null){
+                    queue.offer(node.left);
+                }
+                if(node.right != null){
+                    queue.offer(node.right);
+                }
+            }
+            result.add(0,temp);
+        }
+        return result;
     }
 }
