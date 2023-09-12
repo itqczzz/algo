9eab7357a9d67dab590b9984410191aed9c0dd82
qiuchang <itqczzz@163.com>
2023-09-13 00:31:15 +0800

    20230913

--- src/array/ReverseWords.java
+++ src/array/ReverseWords.java
@@ -1,64 +1,91 @@
 package array;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class ReverseWords {
-    public String reverseWords(String s) {
-        StringBuilder sb = new StringBuilder(s);
 
-        // 反转整个字符串
-        reverse(sb, 0, sb.length() - 1);
-
-        // 反转每个单词
-        reverseWords(sb);
-
-        // 清理多余空格
-        return cleanSpaces(sb);
+    // 方法一：使用语言特性
+    public String reverseWords1(String s) {
+        // 去除首尾空格，分割字符串
+        String[] words = s.trim().split(" +");
+        // 反转数组
+        Collections.reverse(Arrays.asList(words));
+        // 拼接字符串
+        return String.join(" ", words);
+    }
+    // 方法二：双指针
+    public String reverseWords2(String s){
+        // 去除首尾空格
+        s = s.trim();
+        // 双指针
+        int i = s.length() - 1;
+        int j = i;
+        StringBuilder sb = new StringBuilder();
+        while (i >= 0){
+            // 搜索首个空格
+            while (i >= 0 && s.charAt(i) != ' ') i--;
+            // 添加单词
+            sb.append(s.substring(i + 1, j + 1) + " ");
+            // 跳过单词间空格
+            while (i >= 0 && s.charAt(i) == ' ') i--;
+            // j 指向下个单词的尾字符
+            j = i;
+        }
+        // 转化为字符串并返回
+        return sb.toString().trim();
+    }
+    // 方法三：双指针（原地算法）
+    public String reverseWords3(String s) {
+        // 去除首尾空格
+        s = s.trim();
+        // 双指针
+        int i = s.length() - 1;
+        int j = i;
+        char[] chars = s.toCharArray();
+        while (i >= 0){
+            // 搜索首个空格
+            while (i >= 0 && chars[i] != ' ') i--;
+            // 添加单词
+            reverse(chars, i + 1, j);
+            // 跳过单词间空格
+            while (i >= 0 && chars[i] == ' ') i--;
+            // j 指向下个单词的尾字符
+            j = i;
+        }
+        // 转化为字符串并返回
+        return new String(chars).trim();
     }
 
-    private void reverseWords(StringBuilder sb) {
-        int i = 0;
-        int j = 0;
+    //4. 双指针（原地算法优化）
+    public String reverseWords4(String s) {
+        // 去除首尾空格
+        s = s.trim();
+        // 双指针
+        int i = s.length() - 1;
+        int j = i;
+        char[] chars = s.toCharArray();
+        while (i >= 0){
+            // 搜索首个空格
+            while (i >= 0 && chars[i] != ' ') i--;
+            // 添加单词
+            reverse(chars, i + 1, j);
+            // 跳过单词间空格
+            while (i >= 0 && chars[i] == ' ') i--;
+            // j 指向下个单词的尾字符
+            j = i;
+        }
+        // 转化为字符串并返回
+        return new String(chars, 0, j + 1);
+    }
 
-        while (i < sb.length()) {
-            while (i < j || i < sb.length() && sb.charAt(i) == ' ') i++; // 跳过空格
-            while (j < i || j < sb.length() && sb.charAt(j) != ' ') j++; // 跳过非空格
-
-            // 反转单词
-            reverse(sb, i, j - 1);
+    private void reverse(char[] chars, int i, int j) {
+        while (i < j){
+            char temp = chars[i];
+            chars[i++] = chars[j];
+            chars[j--] = temp;
         }
     }
 
-    private String cleanSpaces(StringBuilder sb) {
-        int i = 0;
-        int j = 0;
 
-        while (j < sb.length()) {
-            while (j < sb.length() && sb.charAt(j) == ' ') j++; // 跳过空格
-            while (j < sb.length() && sb.charAt(j) != ' ') sb.setCharAt(i++, sb.charAt(j++)); // 保留非空格
-
-            // 在每个单词后面添加一个空格（除了最后一个单词）
-            if (j < sb.length()) sb.setCharAt(i++, ' ');
-        }
-
-        // 删除多余的字符
-        sb.setLength(i - 1); // 更新长度时，需要减去 1 以去掉最后一个单词后添加的空格
-
-        return sb.toString();
-    }
-
-    private void reverse(StringBuilder sb, int i, int j) {
-        while (i < j) {
-            char temp = sb.charAt(i);
-            sb.setCharAt(i++, sb.charAt(j));
-            sb.setCharAt(j--, temp);
-        }
-    }
-    
-    public static void main(String[] args) {
-        String str = "  hello world  ";
-        ReverseWords reverseWords = new ReverseWords();
-        String s = reverseWords.reverseWords(str);
-        System.out.println(s);
-    }
 }
